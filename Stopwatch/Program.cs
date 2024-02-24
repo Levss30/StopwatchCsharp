f8fbfd6285feca0f406dc0c23885bbb6d16d02b9
Lucas Levasseur <130407210+Levss30@users.noreply.github.com>
2024-02-24 12:45:58 -0300

    Add files via upload

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+
+Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("S = Segundos  (60s)");
+    Console.WriteLine("M = Minutos (1m)");
+    Console.WriteLine("0 = Sair");
+    Console.WriteLine("Quanto tempo deseja contar ?");
+
+    string data = Console.ReadLine().ToLower();
+    char type = char.Parse(data.Substring(data.Length - 1,1));
+    int time = int.Parse(data.Substring(0, data.Length - 1));
+    int multiplier = 1;
+
+    if(type == 'm')
+        multiplier = 60;
+
+    if(time == 0)
+        System.Environment.Exit(0);
+
+    Start(time * multiplier);
+}
+
+static void Start(int time)
+{
+    int currentTime = 0;
+
+    while(currentTime != time)
+    {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+    }
+
+    Console.Clear();
+    Console.WriteLine("Stopwatch finalizado!!");
+    Thread.Sleep(2500);
+}
